003307397782f2ea5e072fd3cf6f183ab743f146
Oksanka25 <chekoksa25@gmail.com>
2022-04-11 14:47:04 -0400

    Cleaned extra console.logs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const suits = ["♦️", "♥️", "♠️", "♣️"];
-const values = [6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 let deck = [];
 let player1Stack = [];
 let player2Stack = [];
@@ -17,6 +17,10 @@ let rules = document.querySelector(".rules");
 let playedCard1, playedCard2, player
 
 let myValues = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
     6: 6,
     7: 7,
     8: 8,
@@ -56,7 +60,7 @@ function splitDeck() {
     }
     stack1.innerText = player1Stack.length;
 
-    for (let i = 18; i < deck.length; i++) {
+    for (let i = 26; i < deck.length; i++) {
         player2Stack.push(deck[i]);
     }
     stack2.innerText = player2Stack.length;
@@ -68,31 +72,24 @@ function splitDeck() {
 
 }
 
-// console.log(player2Stack, player1Stack);
 
 stack1.addEventListener("click", () => {
     if (isInWar === false) {
-        playedCard1 = player1Stack.shift()
+        playedCard1 = player1Stack.shift();
         p2Card.innerText = "";
         p1Card.innerText = "";
         p1Card.innerHTML = playedCard1.suit + playedCard1.rank;
         stack1.style.backgroundImage = "url(https://openclipart.org/image/400px/180064)";
         stack1.innerText = player1Stack.length;
-        // console.log(playedCard1);
         message.innerHTML = "Player 2, click on your stack to make a move";
     } else if (isInWar === true) {
-        console.log(player1Stack);
         warPlayedCard1 = player1Stack.splice(0, 4);
-        console.log(player1Stack);
-        console.log(warPlayedCard1);
         p1Card.innerHTML = warPlayedCard1[3].suit + warPlayedCard1[3].rank;
-        // console.log(player1Stack.length);
-        // console.log(player1Stack);
         stack1.innerText = player1Stack.length;
         message.innerHTML = "Player 2, click your stack to add 4 more cards"
-        console.log("Player 2, click your stack to add 4 more cards")
+
     } else {
-        console.log("bla");
+        console.log("Something went wrong");
     }
 })
 
@@ -102,20 +99,14 @@ stack2.addEventListener("click", () => {
         p2Card.innerHTML = playedCard2.suit + playedCard2.rank;
         stack2.style.backgroundImage = "url(https://openclipart.org/image/400px/143575)";
         stack2.innerText = player2Stack.length;
-        // console.log(playedCard2);
         comparison();
     } else if (isInWar === true) {
-        console.log(player2Stack);
         warPlayedCard2 = player2Stack.splice(0, 4);
-        console.log(player2Stack);
-        console.log(warPlayedCard2);
         p2Card.innerHTML = warPlayedCard2[3].suit + warPlayedCard2[3].rank;
-        // console.log(player2Stack.length);
-        // console.log(player2Stack);
         stack2.innerText = player2Stack.length;
         warcomparison();
     } else {
-        console.log("bla bla");
+        console.log("Ooops... we have a problem");
     }
 })
 
@@ -133,7 +124,6 @@ rules.addEventListener("click", () => {
 start.addEventListener("click", () => {
     if (gameOn === true) {
         shuffleDeck(); splitDeck();
-        // console.log(player1Stack);
     } else {
         message.innerHTML = "The game has already started";
     }
@@ -150,26 +140,16 @@ function shuffleDeck() {
         deck[i] = deck[j];
         deck[j] = temp;
     }
-    // console.log(deck);
 }
 
-function comparison(event) {
-    // event.preventDefault()
+function comparison() {
     if (playedCard1.value > playedCard2.value) {
-        console.log(player1Stack);
         player1Stack.push(playedCard1, playedCard2);
-        console.log(player1Stack);
-        // p2Card.innerText = "";
-        // p1Card.innerText = "";
         message.innerHTML = "Player 1 has a higher card. Player 1, your move is next";
         stack1.innerText = player1Stack.length;
 
     } else if (playedCard1.value < playedCard2.value) {
-        console.log(player2Stack);
         player2Stack.push(playedCard1, playedCard2);
-        console.log(player2Stack);
-        // p2Card.innerText = "";
-        // p1Card.innerText = "";
         message.innerHTML = "Player 2 has a higher card. Player 1, your move is next";
         stack2.innerText = player2Stack.length;
 
